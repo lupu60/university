ab52f3fefb0a61e57bdbe0bc80344692e6772d8b
Lupu Bogdan <lupu60@gmail.com>
2015-05-19 18:29:51 +0300

    web controller done

---
+++ car_server/gruntfile.js
@@ -0,0 +1,78 @@
+module.exports = function(grunt) {
+    require('load-grunt-tasks')(grunt);
+
+    var BUILD_DIR = './public/';
+    var BUILD_DIR_JS = BUILD_DIR + 'javascripts/';
+    var BUILD_DIR_CSS = BUILD_DIR + 'stylesheets/';
+    var BUILD_DIR_IMAGES = BUILD_DIR + 'images/';
+    var BUILD_DIR_LESS = BUILD_DIR + 'less/';
+    // =========================================================
+    var BUILD_FILE_JS = BUILD_DIR_JS + 'libs.js';
+    // =========================================================
+    var BUILD_FILES_LESS = BUILD_DIR_LESS + '*.less';
+    // =========================================================
+    // =========================================================
+    var JQUERY = './node_modules/jquery/dist/jquery.js';
+    var BOOTSTRAP_JS = './node_modules/bootstrap/dist/js/bootstrap.js';
+    // =========================================================
+    // configure the tasks
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        asciify: {
+            banner: {
+                text: '<%= pkg.name %> ',
+                options: {
+                    font: 'doom',
+                    log: true
+                },
+            },
+        },
+        // =============================================
+        // =            Core Tasks                     =
+        // =============================================
+        copy: {
+            bootstrap_fonts: {
+                files: [{
+                    expand: true,
+                    cwd: './node_modules/bootstrap/fonts/',
+                    src: ['**'],
+                    dest: BUILD_DIR + 'fonts'
+                }, ],
+            },
+        },
+        concat: {
+            options: {
+                sourceMap: true,
+            },
+            libs: {
+                src: [JQUERY, BOOTSTRAP_JS],
+                dest: BUILD_FILE_JS,
+            },
+        },
+        less: {
+            development: {
+                options: {
+                    compress: false,
+                    cleancss: false,
+                    sourceMap: true,
+                    sourceMapURL: './style.css.map',
+                    sourceMapRootpath: '../../',
+                },
+                files: {
+                    "./public/stylesheets/style.css": "./public/less/style.less",
+                    "./public/stylesheets/controller.css": "./public/less/controller.less",
+                },
+            },
+        },
+        watch: {
+            stylesless: {
+                options: {
+                    livereload: false
+                },
+                files: [BUILD_FILES_LESS],
+                tasks: ['less:development']
+            },
+        },
+    });
+    grunt.registerTask('default', ['asciify','watch']);
+};
---
+++ car_server/public/javascripts/controller.js
@@ -0,0 +1,89 @@
+var socket = io();
+$(function() {
+    var move = {
+        "data": '',
+        "val": ''
+    };
+
+    function send(move) {
+        socket.emit('moving', {move});
+        return false;
+    }
+    $('#up').bind('touchstart', function() {
+        $(this).css({
+            'background-color': '#1AFF00'
+        });
+        move.data = "true";
+        move.val = "up";
+        send(move);
+    });
+    $('#up').bind('touchend', function() {
+        $(this).css({
+            'background-color': '#F1F1F1'
+        });
+        move.data = "false";
+        move.val = "up";
+        send(move);
+    });
+    $('#down').bind('touchstart', function() {
+        $(this).css({
+            'background-color': '#1AFF00'
+        });
+        move.data = "true";
+        move.val = "down";
+        send(move);
+    });
+    $('#down').bind('touchend', function() {
+        $(this).css({
+            'background-color': '#F1F1F1'
+        });
+        move.data = "false";
+        move.val = "down";
+        send(move);
+    });
+    $('#left').bind('touchstart', function() {
+        $(this).css({
+            'background-color': '#1AFF00'
+        });
+        move.data = "true";
+        move.val = "left";
+        send(move);
+    });
+    $('#left').bind('touchend', function() {
+        $(this).css({
+            'background-color': '#F1F1F1'
+        });
+        move.data = "false";
+        move.val = "left";
+        send(move);
+    });
+    $('#right').bind('touchstart', function() {
+        $(this).css({
+            'background-color': '#1AFF00'
+        });
+        move.data = "true";
+        move.val = "right";
+        send(move);
+    });
+    $('#right').bind('touchend', function() {
+        $(this).css({
+            'background-color': '#F1F1F1'
+        });
+        move.data = "false";
+        move.val = "right";
+        send(move);
+    });
+    $('#stop').bind('touchstart', function() {
+        $(this).css({
+            'background-color': 'red'
+        });
+        move.data = "true";
+        move.val = "stop";
+        send(move);
+    });
+    $('#stop').bind('touchend', function() {
+        $(this).css({
+            'background-color': '#F1F1F1'
+        });
+    });
+});
