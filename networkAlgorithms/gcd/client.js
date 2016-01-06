be766f48d1df9158b6a5d7d73e4362421aed1f1d
Lupu Bogdan <lupu60@gmail.com>
2016-01-06 11:54:04 +0200

    fix workspace

--- networkAlgorithms/gcd/client.js
+++
@@ -1,7 +0,0 @@
-var connectTo = require('./lib/connectTo.js');
-setTimeout(function() {
-    connectTo.startClient();
-}, 1000);
-setTimeout(function() {
-    connectTo.connectToServer();
-}, 2000);
