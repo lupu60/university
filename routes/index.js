d3ad51ae58e7b5ca13ee1dac0f6b43d57cceff83
lupu <lupu60@gmail.com>
2016-01-09 19:13:13 +0200

    crud done for winter and summer

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
+var squel = require("squel");
+squel.useFlavour('postgres');
+var connect_string = 'postgres://oerwxunzakxntm:zMVQKkdOOHBxhRLzntgOSTdEem@ec2-54-83-52-71.compute-1.amazonaws.com:5432/df4a0g4bh0chk4?ssl=true';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,10 +12,19 @@ router.get('/', function(req, res, next) {
   });
 });
 
-pg.connect('postgres://oerwxunzakxntm:zMVQKkdOOHBxhRLzntgOSTdEem@ec2-54-83-52-71.compute-1.amazonaws.com:5432/df4a0g4bh0chk4?ssl=true', function(err, client) {
+// var sql = require('sql');
+// sql.setDialect('postgres');
+//
+// //first we define our tables
+// var winter_tire = sql.define({
+//   name: 'winter_tire',
+//   columns: ['id', 'brand', 'size', 'profile','speed_rating','quantity','price']
+// });
+
+pg.connect(connect_string, function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
-  // client.query('DROP TABLE foo', function(err, result) {
+  // client.query('DROP TABLE winter_tire', function(err, result) {
   //   if (err)
   //    { console.error(err);  }
   //   else
@@ -24,48 +36,160 @@ pg.connect('postgres://oerwxunzakxntm:zMVQKkdOOHBxhRLzntgOSTdEem@ec2-54-83-52-71
 
 // List winter_tire
 router.get('/winter_tire', function(req, res, next) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  pg.connect(connect_string, function(err, client, done) {
     client.query('SELECT * FROM winter_tire', function(err, result) {
       done();
       if (err) {
         console.error(err);
         response.send("Error " + err);
       } else {
-              res.send('{"tires":'+JSON.stringify(result.rows)+'}')
+        res.send('{"tires":' + JSON.stringify(result.rows) + '}')
+      }
+    });
+  });
+});
+//INSERT winter_tire
+router.put('/winter_tire', function(req, res, next) {
+  var query_text = squel.insert()
+    .into("winter_tire")
+    .set("brand", req.body.brand)
+    .set("size", req.body.size)
+    .set("profile", req.body.profile)
+    .set("speed_rating", req.body.speed_rating)
+    .set("quantity", req.body.quantity)
+    .set("price", req.body.price)
+    .toString();
+  pg.connect(connect_string, function(err, client, done) {
+    client.query(query_text, function(err, result) {
+      done();
+      if (err) {
+        console.error(err);
+        res.send(err);
+      } else {
+        res.send("ok")
+      }
+    });
+  });
+});
+//UDPATE winter_tire
+router.post('/winter_tire', function(req, res, next) {
+  var query_text = squel.update()
+    .table("winter_tire")
+    .set("brand", req.body.brand)
+    .set("size", req.body.size)
+    .set("profile", req.body.profile)
+    .set("speed_rating", req.body.speed_rating)
+    .set("quantity", req.body.quantity)
+    .set("price", req.body.price)
+    .where("id=" + req.body.id)
+    .toString();
+  pg.connect(connect_string, function(err, client, done) {
+    client.query(query_text, function(err, result) {
+      done();
+      if (err) {
+        console.error(err);
+        res.send(err);
+      } else {
+        res.send("ok")
+      }
+    });
+  });
+});
+//DELETE winter_tire
+router.delete('/winter_tire', function(req, res, next) {
+  var query_text = squel.delete()
+    .from("winter_tire").where("id=" + req.body.id)
+    .toString();
+  pg.connect(connect_string, function(err, client, done) {
+    client.query(query_text, function(err, result) {
+      done();
+      if (err) {
+        console.error(err);
+        res.send(err);
+      } else {
+        res.send("ok")
       }
     });
   });
 });
 
-router.put('/winter_tire', function(req, res, next) {
-  console.log(req.body.brand);
-  res.send(req.body);
-  var query_text = 'INSERT INTO winter_tire (brand ,size ,profile ,speed_rating ,quantity , price ) values (' + req.body.brand + ',' + req.body.size + ',' + req.body.profile + ',' + req.body.speed_rating + ',' + req.body.quantity + ', ' + req.body.price + ');'
-  console.log(query_text);
-
-  pg.connect('postgres://oerwxunzakxntm:zMVQKkdOOHBxhRLzntgOSTdEem@ec2-54-83-52-71.compute-1.amazonaws.com:5432/df4a0g4bh0chk4?ssl=true', function(err, client, done) {
-    client.query(query_text, function(err, result) {
-      done();
-      if (err)
-       { console.error(err); }
-      else
-       { res.send(result.rows)}
-    });
-  });
-});
 
 router.get('/summer_tire', function(req, res, next) {
-  pg.connect('postgres://oerwxunzakxntm:zMVQKkdOOHBxhRLzntgOSTdEem@ec2-54-83-52-71.compute-1.amazonaws.com:5432/df4a0g4bh0chk4?ssl=true', function(err, client, done) {
+  pg.connect(connect_string, function(err, client, done) {
     client.query('SELECT * FROM summer_tire', function(err, result) {
       done();
       if (err) {
         console.error(err);
         response.send("Error " + err);
       } else {
-        res.send('{"tires":'+result.rows+'}')
+        res.send('{"tires":' + JSON.stringify(result.rows) + '}')
       }
     });
   });
-
 });
+//INSERT summer_tire
+router.put('/summer_tire', function(req, res, next) {
+  var query_text = squel.insert()
+    .into("summer_tire")
+    .set("brand", req.body.brand)
+    .set("size", req.body.size)
+    .set("profile", req.body.profile)
+    .set("speed_rating", req.body.speed_rating)
+    .set("quantity", req.body.quantity)
+    .set("price", req.body.price)
+    .toString();
+  pg.connect(connect_string, function(err, client, done) {
+    client.query(query_text, function(err, result) {
+      done();
+      if (err) {
+        console.error(err);
+        res.send(err);
+      } else {
+        res.send("ok")
+      }
+    });
+  });
+});
+//UDPATE summer_tire
+router.post('/summer_tire', function(req, res, next) {
+  var query_text = squel.update()
+    .table("summer_tire")
+    .set("brand", req.body.brand)
+    .set("size", req.body.size)
+    .set("profile", req.body.profile)
+    .set("speed_rating", req.body.speed_rating)
+    .set("quantity", req.body.quantity)
+    .set("price", req.body.price)
+    .where("id=" + req.body.id)
+    .toString();
+  pg.connect(connect_string, function(err, client, done) {
+    client.query(query_text, function(err, result) {
+      done();
+      if (err) {
+        console.error(err);
+        res.send(err);
+      } else {
+        res.send("ok")
+      }
+    });
+  });
+});
+//DELETE summer_tire
+router.delete('/summer_tire', function(req, res, next) {
+  var query_text = squel.delete()
+    .from("summer_tire").where("id=" + req.body.id)
+    .toString();
+  pg.connect(connect_string, function(err, client, done) {
+    client.query(query_text, function(err, result) {
+      done();
+      if (err) {
+        console.error(err);
+        res.send(err);
+      } else {
+        res.send("ok")
+      }
+    });
+  });
+});
+
 module.exports = router;
