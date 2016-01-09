0eeae26ef5c8e56e55111b953f8ed2bfc75968a1
lupu <lupu60@gmail.com>
2016-01-09 18:08:34 +0200

    list winter and summer tires

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
@@ -8,14 +9,61 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.get('/db', function(req, res, next) {
-pg.connect(process.env.DATABASE_URL+ '?ssl=true', function(err, client, done) {
+pg.connect('postgres://oerwxunzakxntm:zMVQKkdOOHBxhRLzntgOSTdEem@ec2-54-83-52-71.compute-1.amazonaws.com:5432/df4a0g4bh0chk4?ssl=true', function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+  // client.query('DROP TABLE foo', function(err, result) {
+  //   if (err)
+  //    { console.error(err);  }
+  //   else
+  //    { res.send(result.rows)}
+  // });
+});
+
+
+
+// List winter_tire
+router.get('/winter_tire', function(req, res, next) {
+  pg.connect('postgres://oerwxunzakxntm:zMVQKkdOOHBxhRLzntgOSTdEem@ec2-54-83-52-71.compute-1.amazonaws.com:5432/df4a0g4bh0chk4?ssl=true', function(err, client, done) {
     client.query('SELECT * FROM winter_tire', function(err, result) {
       done();
+      if (err) {
+        console.error(err);
+        response.send("Error " + err);
+      } else {
+              res.send('{"tires":'+JSON.stringify(result.rows)+'}')
+      }
+    });
+  });
+});
+
+router.put('/winter_tire', function(req, res, next) {
+  console.log(req.body.brand);
+  res.send(req.body);
+  var query_text = 'INSERT INTO winter_tire (brand ,size ,profile ,speed_rating ,quantity , price ) values (' + req.body.brand + ',' + req.body.size + ',' + req.body.profile + ',' + req.body.speed_rating + ',' + req.body.quantity + ', ' + req.body.price + ');'
+  console.log(query_text);
+
+  pg.connect('postgres://oerwxunzakxntm:zMVQKkdOOHBxhRLzntgOSTdEem@ec2-54-83-52-71.compute-1.amazonaws.com:5432/df4a0g4bh0chk4?ssl=true', function(err, client, done) {
+    client.query(query_text, function(err, result) {
+      done();
       if (err)
-       { console.error(err); response.send("Error " + err); }
+       { console.error(err); }
       else
        { res.send(result.rows)}
+    });
+  });
+});
+
+router.get('/summer_tire', function(req, res, next) {
+  pg.connect('postgres://oerwxunzakxntm:zMVQKkdOOHBxhRLzntgOSTdEem@ec2-54-83-52-71.compute-1.amazonaws.com:5432/df4a0g4bh0chk4?ssl=true', function(err, client, done) {
+    client.query('SELECT * FROM summer_tire', function(err, result) {
+      done();
+      if (err) {
+        console.error(err);
+        response.send("Error " + err);
+      } else {
+        res.send('{"tires":'+result.rows+'}')
+      }
     });
   });
 
