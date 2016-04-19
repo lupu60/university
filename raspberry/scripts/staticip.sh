891f90a98bed1c9703bd94e27b98c80321c7ee28
bogdan lupu <lupu60@gmail.com>
2016-04-19 22:39:50 +0300

    rasoberry tools and setup

--- raspberry/scripts/staticip.sh
+++ raspberry/scripts/staticip.sh
@@ -5,9 +5,9 @@ if [ $(id -u) -ne 0 ]; then
 fi
 
 # Configure management IP
-MANAGEMENT_IP=$(whiptail --nocancel --inputbox "Please enter the management IP" 20 60 "192.168.123.XXX" 3>&1 1>&2 2>&3)
+MANAGEMENT_IP=$(whiptail --nocancel --inputbox "Please enter the management IP" 20 60 "192.168.0.XXX" 3>&1 1>&2 2>&3)
 if [ $? -eq 0 ]; then
-  sed -i "s/#MANAGEMENT_INTERFACE/iface eth0:1 inet static/g" /etc/network/interfaces
+  sed -i "s/#MANAGEMENT_INTERFACE/iface wlan0:1 inet static/g" /etc/network/interfaces
   sed -i "s/#MANAGEMENT_IP/address $MANAGEMENT_IP/g" /etc/network/interfaces
   sed -i "s/#MANAGEMENT_NETMASK/netmask 255.255.255.0/g" /etc/network/interfaces
 fi
