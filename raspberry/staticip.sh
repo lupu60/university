e2adc2a92b5f82038c9f29a2450a50f7b5f6a54e
Bogdan Lupu <bogdan.lupu@elektrobit.com>
2016-04-19 16:22:14 +0300

    add respi res

---
+++ raspberry/staticip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ $(id -u) -ne 0 ]; then
+  printf "Script must be run as root. Try 'sudo $0'\n"
+  exit 1
+fi
+
+# Configure management IP
+MANAGEMENT_IP=$(whiptail --nocancel --inputbox "Please enter the management IP" 20 60 "192.168.123.XXX" 3>&1 1>&2 2>&3)
+if [ $? -eq 0 ]; then
+  sed -i "s/#MANAGEMENT_INTERFACE/iface eth0:1 inet static/g" /etc/network/interfaces
+  sed -i "s/#MANAGEMENT_IP/address $MANAGEMENT_IP/g" /etc/network/interfaces
+  sed -i "s/#MANAGEMENT_NETMASK/netmask 255.255.255.0/g" /etc/network/interfaces
+fi
