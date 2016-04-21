e25bdf3f4c50e2d41cd2d98e07d16f13a4412aa7
bogdan lupu <lupu60@gmail.com>
2016-04-22 01:30:58 +0300

    modular js keyboard_controller

--- public/javascripts/keyboard_controller.js
+++ public/javascripts/keyboard_controller.js
@@ -1,81 +1,88 @@
-var socket = io.connect(':4000');
-var move = {
-    "data": '',
-    "val": ''
-};
-
-function send(move) {
-    socket.emit('moving', { move: move });
-    return false;
-}
-
-function startPeristentVibrate(level) {
-    navigator.vibrate(level);
-}
-
-function stopVibrate() {
-    navigator.vibrate(0);
-    navigator.vibrate([]);
-}
-
-function touchstart(selector) {
-    startPeristentVibrate(5000);
-    $(selector).css({
-        'background-color': '#1AFF00'
-    });
-    move.data = "true";
-    move.val = selector[0].id;
-    send(move);
-}
-
-function touchend(selector) {
-    stopVibrate();
-    $(selector).css({
-        'background-color': '#F1F1F1'
-    });
-    move.data = "false";
-    move.val = selector[0].id;
-    send(move);
-}
-
-$(document).ready(function() {
-    $('#up').bind('touchstart', function() {
-        touchstart($(this));
-    });
-    $('#up').bind('touchend', function() {
-        touchend($(this));
-    });
-    $('#down').bind('touchstart', function() {
-        touchstart($(this));
-    });
-    $('#down').bind('touchend', function() {
-        touchend($(this));
-    });
-    $('#left').bind('touchstart', function() {
-        touchstart($(this));
-    });
-    $('#left').bind('touchend', function() {
-        touchend($(this));
-    });
-    $('#right').bind('touchstart', function() {
-        touchstart($(this));
-    });
-    $('#right').bind('touchend', function() {
-        touchend($(this));
-    });
-    $('#stop').bind('touchstart', function() {
-        startPeristentVibrate(5000);
-        $(this).css({
-            'background-color': 'red'
-        });
-        move.data = "true";
-        move.val = "stop";
-        send(move);
-    });
-    $('#stop').bind('touchend', function() {
-        stopVibrate();
-        $(this).css({
-            'background-color': '#F1F1F1'
-        });
-    });
-});
+(function() {
+    var socket = io.connect(':4000');
+    var keyboardController = {
+        move: {
+            data: "",
+            val: "",
+        },
+        init: function() {
+            this.cacheDom();
+            this.bindEvents();
+        },
+        cacheDom: function() {
+            this.$up = $('#up');
+            this.$down = $('#down');
+            this.$left = $('#left');
+            this.$right = $('#right');
+            this.$stop = $('#stop');
+        },
+        bindEvents: function() {
+            this.$up.bind('touchstart', function() {
+                keyboardController.touchStart($(this));
+            });
+            this.$up.bind('touchend', function() {
+                keyboardController.touchEnd($(this));
+            });
+            this.$down.bind('touchstart', function() {
+                keyboardController.touchStart($(this));
+            });
+            this.$down.bind('touchend', function() {
+                keyboardController.touchEnd($(this));
+            });
+            this.$left.bind('touchstart', function() {
+                keyboardController.touchStart($(this));
+            });
+            this.$left.bind('touchend', function() {
+                keyboardController.touchEnd($(this));
+            });
+            this.$right.bind('touchstart', function() {
+                keyboardController.touchStart($(this));
+            });
+            this.$right.bind('touchend', function() {
+                keyboardController.touchEnd($(this));
+            });
+            this.$stop.bind('touchstart', function() {
+                keyboardController.touchStart($(this));
+            });
+            this.$stop.bind('touchend', function() {
+                keyboardController.touchEnd($(this));
+            });
+        },
+        render: function(type, selector) {
+            if (type === "start") {
+                if (selector[0].id === "stop") {
+                    selector.css({ 'background-color': '#840606' });
+                } else {
+                    selector.css({ 'background-color': '#1AFF00' });
+                }
+            } else {
+                selector.css({ 'background-color': '#F1F1F1' });
+            }
+        },
+        sendMoveToBackend: function() {
+            socket.emit('moving', { move: this.move });
+        },
+        startPeristentVibrate: function(level) {
+            navigator.vibrate(level);
+        },
+        stopVibrate: function() {
+            navigator.vibrate(0);
+            navigator.vibrate([]);
+        },
+        touchStart: function(selector) {
+            this.startPeristentVibrate(5000);
+            this.render("start", selector);
+            this.move.data = "true";
+            this.move.val = selector[0].id;
+            this.sendMoveToBackend();
+        },
+        touchEnd: function(selector) {
+            this.stopVibrate();
+            this.render("end", selector);
+            this.move.data = "false";
+            this.move.val = selector[0].id;
+            this.sendMoveToBackend();
+        }
+    }
+    keyboardController.init();
+})();
