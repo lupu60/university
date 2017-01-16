4dd0aea5a475b9f83deccdd04ad69d155bd00356
Lupu <lupu60@gmail.com>
2017-01-16 21:23:14 +0200

    add sb admin

--- src/main/resources/static/js/app.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $.ajax({
-        url: "/webapi"
-    }).then(function(data, status, jqxhr) {
-       $('#hello').append(data);
-       console.log(jqxhr);
-    });
-});
---
+++ src/main/resources/static/scripts/controllers/main.js
@@ -0,0 +1,22 @@
+'use strict';
+/**
+ * @ngdoc function
+ * @name sbAdminApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sbAdminApp
+ */
+angular.module('sbAdminApp').controller('MainCtrl', function($scope, $position)
+{
+    $(document).ready(function()
+    {
+        $.ajax(
+        {
+            url: "/webapi"
+        }).then(function(data, status, jqxhr)
+        {
+            $('#hello').append(data);
+            console.log(jqxhr);
+        });
+    });
+});
