020cb05921b31f69260bd44539ad4aa706f39883
Lupu <lupu60@gmail.com>
2017-02-26 11:55:26 +0200

    add model for PatientSteps

---
+++ src/main/java/com/gnp/ioth/model/PatientSteps.java
@@ -0,0 +1,31 @@
+package com.gnp.ioth.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "patient_steps")
+public class PatientSteps {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @OneToMany
+  @JoinColumn(name = "patientId", nullable = false)
+  private Patient patient;
+  
+  @Column(name = "steps", nullable = false)
+  private int steps;
+
+  @Column(name = "timestamp", nullable = false)
+  private Timestamp timestamp;
+}
