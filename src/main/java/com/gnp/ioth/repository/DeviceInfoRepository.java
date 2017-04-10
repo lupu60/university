824b48a316c59c4aa89f44ad444ba1ba8f898180
Lupu <lupu60@gmail.com>
2017-04-10 18:16:53 +0200

    del some junk

--- src/main/java/com/gnp/ioth/repository/DeviceInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gnp.ioth.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gnp.ioth.model.DeviceInfo;
-
-public interface DeviceInfoRepository extends JpaRepository<DeviceInfo, Long>{
-
-}
