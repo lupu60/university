416bc1b8584131c848f2468a1f6a5b09a1178cd6
Lupu <lupu60@gmail.com>
2017-04-12 12:09:53 +0200

    device montitor entity

--- src/main/java/com/gnp/ioth/model/DeviceMonitoring.java
+++ src/main/java/com/gnp/ioth/model/DeviceMonitoring.java
@@ -1,10 +1,14 @@
 package com.gnp.ioth.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "device-monitoring")
 public class DeviceMonitoring {
 
   @Id
