5505d1b041a8aec54b57a3962c5b6aa10f03685e
Bogdan Lupu <lupu60@gmail.com>
2017-04-21 15:57:19 +0200

    add device info and monitor

--- src/main/java/com/gnp/ioth/model/DeviceInfo.java
+++ src/main/java/com/gnp/ioth/model/DeviceInfo.java
@@ -1,5 +1,6 @@
 package com.gnp.ioth.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,13 +9,16 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "device_monitoring")
-public class DeviceMonitoring {
+@Table(name = "device_info")
+public class DeviceInfo {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
-
+  
+  @Column(name = "firmware_version", unique = false, nullable = false)
+  private String firmwareVersion;
+  
   @ManyToOne
   private SmartBand smartBand;
 }
