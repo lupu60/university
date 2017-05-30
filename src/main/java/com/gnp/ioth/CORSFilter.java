e34a9268a442dfc19837d4eb7ed929ac9b03c7f4
Bogdan Lupu <lupu60@gmail.com>
2017-05-30 20:17:23 +0200

    fix: sonarqube comment

--- src/main/java/com/gnp/ioth/CORSFilter.java
+++ src/main/java/com/gnp/ioth/CORSFilter.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 public class CORSFilter implements Filter {
 
+  @Override
   public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
       throws IOException, ServletException {
     HttpServletResponse response = (HttpServletResponse) res;
